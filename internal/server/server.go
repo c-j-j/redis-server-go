2b39a2c46b86674b3a9bb66630d477f065c2d0bc
Chris Jordan <chrisjordan1987@gmail.com>
2025-01-03 13:49:25 +0000

    basic get/set

--- internal/server/server.go
+++ internal/server/server.go
@@ -11,6 +11,17 @@ import (
 	"strings"
 )
 
+func handleMessages(inMemoryDB map[string]string, writeChannel chan WriteRequest) {
+	for nextMessage := range writeChannel {
+		inMemoryDB[nextMessage.key] = nextMessage.value
+	}
+}
+
+type WriteRequest struct {
+	key   string
+	value string
+}
+
 func StartServer() {
 	if os.Getenv("VERBOSE") != "" {
 		slog.SetLogLoggerLevel(slog.LevelDebug)
@@ -24,6 +35,12 @@ func StartServer() {
 		os.Exit(-1)
 	}
 	defer listener.Close()
+
+	writeChannel := make(chan WriteRequest)
+	inMemoryDB := make(map[string]string)
+
+	go handleMessages(inMemoryDB, writeChannel)
+
 	fmt.Printf("Server started on port %s\n", port)
 	for true {
 		connection, err := listener.Accept()
@@ -33,11 +50,11 @@ func StartServer() {
 			os.Exit(-1)
 		}
 
-		go handleRequest(connection)
+		go handleRequest(connection, writeChannel, inMemoryDB)
 	}
 }
 
-func handleRequest(connection net.Conn) {
+func handleRequest(connection net.Conn, writeChannel chan WriteRequest, inMemoryDB map[string]string) {
 	defer connection.Close()
 	slog.Debug("Received new connection")
 
@@ -65,13 +82,13 @@ func handleRequest(connection net.Conn) {
 			connection.Write([]byte("+Error parsing request"))
 			continue
 		} else if message.Completed {
-			response := generateResponse(message)
+			response := handleResponse(message, writeChannel, inMemoryDB)
 			connection.Write([]byte(response))
 		}
 	}
 }
 
-func generateResponse(message *message.Message) string {
+func handleResponse(message *message.Message, writeChannel chan WriteRequest, inMemoryDB map[string]string) string {
 	tokens := message.Tokens
 	messageType := tokens[0]
 
@@ -84,6 +101,24 @@ func generateResponse(message *message.Message) string {
 		}
 		echoed := tokens[1]
 		return fmt.Sprintf("$%d\r\n%s\r\n", len(echoed), echoed)
+	case "SET":
+		if len(tokens) != 3 {
+			return "+Expected 2 arguments provided to SET\r\n"
+		}
+		key := tokens[1]
+		value := tokens[2]
+		writeChannel <- WriteRequest{key, value}
+		return "+OK\r\n"
+	case "GET":
+		if len(tokens) != 2 {
+			return "+Expected 1 argument provided to GET\r\n"
+		}
+		key := tokens[1]
+		value, ok := inMemoryDB[key]
+		if !ok {
+			return "$-1\r\n"
+		}
+		return fmt.Sprintf("$%d\r\n%s\r\n", len(value), value)
 	default:
 		return "+UNSUPPORTED\r\n"
 	}
--- test/integration_test.go
+++ test/integration_test.go
@@ -1,19 +1,24 @@
 package test
 
 import (
-	"bufio"
+	"fmt"
 	"net"
 	"redis-go/internal/server"
 	"testing"
 	"time"
 )
 
+var serverStarted = false
+
 func startRedisServer() {
-	go func() {
-		server.StartServer()
-	}()
-	// Give the server time to start
-	time.Sleep(1 * time.Second)
+	if !serverStarted {
+		go func() {
+			server.StartServer()
+			// Give the server time to start
+			time.Sleep(1 * time.Second)
+		}()
+	}
+	serverStarted = true
 }
 
 func TestRedisEchoCommand(t *testing.T) {
@@ -27,33 +32,73 @@ func TestRedisEchoCommand(t *testing.T) {
 
 	// Send ECHO command to the server
 	command := "*2\r\n$4\r\nECHO\r\n$3\r\nHEY\r\n"
+	fmt.Println("a")
 	_, err = conn.Write([]byte(command))
 	if err != nil {
 		t.Fatalf("Failed to send command to Redis server: %v", err)
 	}
 
-	// Read response from the server
-	reader := bufio.NewReader(conn)
-	// Verify the line length
-	lineLength, err := reader.ReadString('\n')
+	fmt.Println("hey")
+	// Read the entire response from the server
+	buffer := make([]byte, 4096)
+	n, err := conn.Read(buffer)
+	response := string(buffer[:n])
+	fmt.Println("bye")
 	if err != nil {
 		t.Fatalf("Failed to read response from Redis server: %v", err)
 	}
 
-	expectedLineLength := "$3\r\n"
-	if lineLength != expectedLineLength {
-		t.Errorf("Unexpected response: got %q, want %q", lineLength, expectedLineLength)
+	expectedResponse := "$3\r\nHEY\r\n"
+	if string(response) != expectedResponse {
+		t.Errorf("Unexpected response: got %q, want %q", response, expectedResponse)
 	}
+}
 
-	// Verify the data
-	dataLine, err := reader.ReadString('\n')
+func TestRedisSetGetCommand(t *testing.T) {
+	startRedisServer()
+
+	conn, err := net.Dial("tcp", "localhost:6379") // Adjust the port if your server uses a different one
 	if err != nil {
-		t.Fatalf("Failed to read response from Redis server: %v", err)
+		t.Fatalf("Failed to connect to Redis server: %v", err)
+	}
+	defer conn.Close()
+
+	// Send SET command to the server
+	setCommand := "*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n"
+	_, err = conn.Write([]byte(setCommand))
+	if err != nil {
+		t.Fatalf("Failed to send SET command to Redis server: %v", err)
 	}
 
-	// Verify the response
-	expectedDataLine := "HEY\r\n"
-	if dataLine != expectedDataLine {
-		t.Errorf("Unexpected response: got %q, want %q", dataLine, expectedDataLine)
+	// Read response for SET command
+	setBuffer := make([]byte, 4096)
+	n, err := conn.Read(setBuffer)
+	setResponse := string(setBuffer[:n])
+	if err != nil {
+		t.Fatalf("Failed to read SET response from Redis server: %v", err)
+	}
+
+	if setResponse != "+OK\r\n" {
+		t.Errorf("Unexpected SET response: got %q, want %q", setResponse, "+OK\r\n")
+	}
+
+	// Send GET command to the server
+	getCommand := "*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n"
+	_, err = conn.Write([]byte(getCommand))
+	if err != nil {
+		t.Fatalf("Failed to send GET command to Redis server: %v", err)
+	}
+
+	// Read response for GET command
+	getBuffer := make([]byte, 4096)
+	n, err = conn.Read(getBuffer)
+	getResponse := string(getBuffer[:n])
+	if err != nil {
+		t.Fatalf("Failed to read GET response from Redis server: %v", err)
+	}
+
+	expectedGetResponse := "$5\r\nvalue\r\n"
+	if getResponse != expectedGetResponse {
+		t.Errorf("Unexpected GET response: got %q, want %q", getResponse, expectedGetResponse)
 	}
 }
